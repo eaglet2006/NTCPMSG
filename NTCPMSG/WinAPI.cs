dfa8b10887d9032b88437497596e7bc4c2d6eed0
eaglet2006_cp <SND\eaglet2006_cp>
2012-09-10 10:18:50 +0000

    V1.0.1.1 Miss a file

---
+++ NTCPMSG/WinAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace NTCPMSG
+{
+    class WinAPI
+    {
+        [DllImport("kernel32.dll")]
+        internal static extern bool SetThreadPriorityBoost(IntPtr hThread,
+           bool DisablePriorityBoost);
+        [DllImport("kernel32.dll")]
+        internal static extern IntPtr GetCurrentThread();
+    }
+}
